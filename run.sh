f8199d24b0d5f79a1d0bfb2e4555724d3a7f5aba
Joshua McKenty <joshua@pistoncloud.com>
2014-08-02 16:18:37 -0700

    Using ENV instead of get_option

--- run.sh
+++ run.sh
@@ -2,8 +2,5 @@ ruby -v
 wget http://go-cli.s3-website-us-east-1.amazonaws.com/releases/v6.3.2/cf-linux-amd64.tgz
 tar -zxvf cf-linux-amd64.tgz
 ./cf api https://api.run.pivotal.io
-org=`get_option organization`
-space=`get_option space`
-./cf login -u $WERCKER_CLOUDFOUNDRY_USERNAME -p $WERCKER_CLOUDFOUNDRY_PASSWORD -o $org -s $space
-appname=`get_option appname`
-./cf push $appname
+./cf login -u $WERCKER_CLOUDFOUNDRY_USERNAME -p $WERCKER_CLOUDFOUNDRY_PASSWORD -o $WERCKER_CLOUD_FOUNDRY_DEPLOY_ORGANIZATION -s $WERCKER_CLOUD_FOUNDRY_DEPLOY_SPACE
+./cf push $WERCKER_CLOUD_FOUNDRY_DEPLOY_APPNAME
